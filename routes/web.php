5319549c6dc715f70fae6ae7b10654b104a13683
Daria Borodina <dasha.borodina2003@mail.ru>
2021-12-08 18:56:49 +0300

    lesson

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use \App\Models\Product;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,28 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    //$product = Product::where('status',true)->get();
+    $product = Product::where('status',true)->where('price','>',1000)->get();
+    /*$data = [
+        'name'=>'mouse',
+        'price'=>1000,
+    ];
+    $product = Product::create($data);*/
+    /*$product = new Product();
+    $product->name = "headphones";
+    $product->price = 3000;
+    $product->save();*/
+   /* $product = Product::find(1);
+    $product->price = 20000;
+    $product->save();*/
+   // dd($product);
+    return view('main');
 });
+Route::get('store', function () {
+    return view('store');
+});
+Route::get('hello', [App\Http\Controllers\SiteController::class,'index']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
